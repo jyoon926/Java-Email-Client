f68932b9c626f36c233c39ee441620911b94c984
Changhoon Yoon <changhoon.yoon@hstattler1-mp92.local>
2018-12-05 17:04:09 -0500

    Update gui

--- src/javafx/Controller.java
+++ src/javafx/Controller.java
@@ -20,12 +20,37 @@ public class Controller implements Initializable
     private TextField textEmail;
     @FXML
     private PasswordField textPassword;
+    @FXML
+    private TextField newName;
+    @FXML
+    private TextField newEmail;
+    @FXML
+    private PasswordField newPassword;
+    @FXML
+    private PasswordField confirmPassword;
+
+    public void startLoginAction(ActionEvent event) throws Exception
+    {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Login.fxml")));
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void startCreateAccountAction(ActionEvent event) throws Exception
+    {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("CreateAccount.fxml")));
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void loginAction(ActionEvent event) throws Exception
     {
         String email = textEmail.getText().toString();
         String password = textPassword.getText().toString();
 
+        //Change this if statement so that it checks if the account is in the database
         if (!email.equals("email@gmail.com") || !password.equals("password"))
         {
             Alert alert = new Alert(AlertType.INFORMATION);
@@ -40,6 +65,34 @@ public class Controller implements Initializable
             stage.setScene(scene);
             stage.show();
             stage.setResizable(false);
+        }
+    }
+
+    public void createAccountAction(ActionEvent event) throws Exception
+    {
+        String name = newName.getText().toString();
+        String email = newEmail.getText().toString();
+        String password = newPassword.getText().toString();
+        String passwordRetype = confirmPassword.getText().toString();
+        if (password.equals(passwordRetype))
+        {
+            //Write code to add the account to the database here
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText("Welcome to DeppeMail, " + name + ". Your email is " + email + "and your password is " + password + ".");
+            alert.showAndWait();
+            Scene scene = new Scene(FXMLLoader.load(getClass().getResource("Application.fxml")));
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+            stage.setResizable(false);
+        }
+        else
+        {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Failed");
+            alert.setHeaderText("Your passwords aren't the same. Try again.");
+            alert.showAndWait();
         }
     }
 
--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -10,11 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-        primaryStage.setResizable(false);
     }
 
     public static void main(String[] args)
