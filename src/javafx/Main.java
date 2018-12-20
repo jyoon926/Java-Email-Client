59394d277961525de56ca495bdfe22561b5384f2
Changhoon Yoon <changhoon.yoon@hstattler1-mp92.icsd.k12.ny.us>
2018-12-20 10:16:10 -0500

    Comment Main.java

--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -6,19 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Runs the Start fxml application
+ */
 public class Main extends Application {
 
+    public static void main(String[] args)
+    {
+        DBAccess.startConnection();
+        launch(args);
+    }
+
+    /**
+     * Sets scene to Start fxml application
+     * @param primaryStage The application window/scene
+     * @throws Exception
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    public static void main(String[] args)
-    {
-        //DBAccess.startConnection();
-        launch(args);
     }
 }
